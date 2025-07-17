482c3d64fdeda8ec2089e5e7c63c772023ed51e5
teutinsa <FloCo12@gmx.net>
2025-07-17 06:54:58 +0200

    Added concept for localization and routing.

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,7 +1,25 @@
 mod titlebar;
+mod localization;
 
 use yew::prelude::*;
+use yew_router::prelude::*;
 use titlebar::TitleBar;
+use localization::Localization;
+
+#[derive(Routable, PartialEq, Eq, Clone)]
+pub enum Route {
+    #[at("/")]
+    Loading,
+    #[not_found]
+    #[at("/404")]
+    NotFound
+}
+
+fn switch(route: Route) -> Html {
+    html! {
+
+    }
+}
 
 #[function_component]
 pub fn App() -> Html {
@@ -9,9 +27,14 @@ pub fn App() -> Html {
 
     });
 
+    let localization = use_state_eq(|| Localization::default());
+
     html! {
-        <>
+        <ContextProvider<Localization> context={(*localization).clone()}>
             <TitleBar on_help={on_help}/>
-        </>
+            <BrowserRouter>
+                <Switch<Route> render={switch}/>
+            </BrowserRouter>
+        </ContextProvider<Localization>>
     }
 }
